cecfecd47ea3953f82040e81c51a61b9cc2da653
mezii <deptrai.team.pro@gmail.com>
2021-10-12 13:21:30 +0700

    zz

--- README.md
+++
@@ -1,2 +0,0 @@
-# repo
-Repos for iOS tweaks ! > 13 
--- package.sh
+++
@@ -1,6 +0,0 @@
-rm ./Packages.bz2
-dpkg-scanpackages -m ./debs > Packages
-bzip2 Packages
-git add .
-git commit -m "zz"
-git push origin main                
